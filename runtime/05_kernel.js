ea7593346caf8ac2a402b9e4a9564a1e75e6a8f4
Peter Zotov <whitequark@whitequark.org>
2011-02-02 02:41:18 +0300

    Fix stub for Kernel.

--- runtime/05_kernel.js
+++ runtime/05_kernel.js
@@ -1,17 +1,19 @@
-$.define_method($c.Object, 'p', -1, function(self, args) {
+$c.Kernel = $c.Object; // while we have no mixins
+
+$.define_method($c.Kernel, 'block_given?', 0, function(self) {
+  return $.block_given() ? Qtrue : Qfalse;
+});
+
+$.define_method($c.Kernel, 'p', -1, function(self, args) {
   for(var i = 0; i < args.length; i++) {
     print($.invoke_method(this, args[i], 'inspect', []));
   }
   return args;
 });
 
-$.define_method($c.Object, 'puts', -1, function(self, args) {
+$.define_method($c.Kernel, 'puts', -1, function(self, args) {
   for(var i = 0; i < args.length; i++) {
     print($.invoke_method(this, args[i], 'to_s', []));
   }
   return Qnil;
-});
-
-$.define_method($c.Object, 'test', 0, function(self) {
-  return Math.random() >= 0.5 ? Qtrue : Qfalse;
 });
