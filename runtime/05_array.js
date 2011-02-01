c2b05798f1a1ae8faebb5a1156997301af70ec93
Peter Zotov <whitequark@whitequark.org>
2011-02-02 01:27:24 +0300

    Minor stdlib updates.

--- runtime/05_array.js
+++ runtime/05_array.js
@@ -1,5 +1,12 @@
 $.define_class('Array', $c.Object);
 
+$.define_method($c.Array, 'each', 0, function(self) {
+  for(var i = 0; i < self.length; i++) {
+    $.yield(this, self[i]);
+  }
+  return self;
+});
+
 $.define_method($c.Array, 'inspect', 0, function(self) {
   var desc = [];
   for(var i = 0; i < self.length; i++) {
--- runtime/05_string.js
+++ runtime/05_string.js
@@ -1,7 +1,15 @@
-$.define_builtin_class('String', $c.Object);
+$.define_class('String', $c.Object);
 
 $.define_method($c.String, 'to_s', 0, function(self) {
   return self;
+});
+
+$.define_method($c.String, 'inspect', 0, function(self) {
+  return '"' + self + '"';
+});
+
+$.define_method($c.String, '+', 1, function(self, other) {
+  return self + other;
 });
 
 String.prototype.klass = $c.String;
