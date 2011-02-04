a91d8a6862a2b2bf5486f80bd371c8f09f9a00fc
Peter Zotov <whitequark@whitequark.org>
2011-02-04 11:20:47 +0300

    Added basic REPL.

---
+++ lib/commands/repl.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+puts "Simple REPL. No line editing is supported. Exit with ^D."
+
+loop do
+  print ">> "
+
+  input = gets
+  break if input.nil?
+
+  begin
+    puts "=> #{eval(input).inspect}"
+  rescue Exception => e
+    puts "#{e.class.name}: #{e.message}"
+    e.backtrace.each { |line| puts "\t#{line}" }
+  end
+end
