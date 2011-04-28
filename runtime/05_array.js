8bc4af715e4c3478707ac172e295c78ddc0f437c
Peter Zotov <whitequark@whitequark.org>
2011-04-28 16:00:44 +0400

    Reordered Array methods to be in alphabetical order.

--- runtime/05_array.js
+++ runtime/05_array.js
@@ -1,37 +1,6 @@
 $.define_class('Array', $c.Object);
 $.module_include($c.Array, $c.Enumerable);
 
-$.define_method($c.Array, 'to_ary', 0, function(self) {
-  return self;
-});
-$.alias_method($c.Array, 'to_a', 'to_ary');
-
-$.define_method($c.Array, 'each', 0, function(self) {
-  for(var i = 0; i < self.length; i++) {
-    this.yield(self[i]);
-  }
-  return self;
-});
-
-$.define_method($c.Array, '[]', 1, function(self, index) {
-  index = this.to_int(index);
-
-  return self[index] == null ? Qnil : self[index];
-});
-
-$.define_method($c.Array, '[]=', 2, function(self, index, value) {
-  index = this.to_int(index);
-
-  self[index] = value;
-
-  return value;
-});
-
-$.define_method($c.Array, 'length', 0, function(self) {
-  return self.length;
-});
-$.alias_method($c.Array, 'size', 'length');
-
 $.define_method($c.Array, '+', 1, function(self, other) {
   other = this.to_ary(other);
 
@@ -70,45 +39,33 @@ $.define_method($c.Array, '-', 1, function(self, other) {
   return result;
 });
 
+$.define_method($c.Array, '[]', 1, function(self, index) {
+  index = this.to_int(index);
+
+  return self[index] == null ? Qnil : self[index];
+});
+
+$.define_method($c.Array, '[]=', 2, function(self, index, value) {
+  index = this.to_int(index);
+
+  self[index] = value;
+
+  return value;
+});
+
 $.define_method($c.Array, 'any?', 0, function(self) {
   return self.length > 0 ? Qtrue : Qfalse;
 });
 
+$.define_method($c.Array, 'each', 0, function(self) {
+  for(var i = 0; i < self.length; i++) {
+    this.yield(self[i]);
+  }
+  return self;
+});
+
 $.define_method($c.Array, 'empty?', 0, function(self) {
   return self.length == 0 ? Qtrue : Qfalse;
-});
-
-$.define_method($c.Array, 'push', 1, function(self, obj) {
-  self.push(obj);
-
-  return self;
-});
-$.alias_method($c.Array, '<<', 'push');
-
-$.define_method($c.Array, 'pop', 0, function(self) {
-  return self.pop();
-});
-
-$.define_method($c.Array, 'unshift', 1, function(self, obj) {
-  self.unshift(obj);
-  return self;
-});
-
-$.define_method($c.Array, 'shift', 0, function(self) {
-  return self.shift();
-});
-
-$.define_method($c.Array, 'uniq!', 0, function(self) {
-  // TODO
-  return self;
-});
-
-$.define_method($c.Array, 'to_s', 0, function(self) {
-  var desc = [];
-  for(var i = 0; i < self.length; i++) {
-    desc.push(this.funcall(self[i], 'inspect').value);
-  }
-  return this.string_new("[" + desc.join(', ') + "]");
 });
 
 $.define_method($c.Array, 'join', -1, function(self, args) {
@@ -124,6 +81,49 @@ $.define_method($c.Array, 'join', -1, function(self, args) {
   }
 
   return this.string_new(output);
+});
+
+$.define_method($c.Array, 'length', 0, function(self) {
+  return self.length;
+});
+$.alias_method($c.Array, 'size', 'length');
+
+$.define_method($c.Array, 'pop', 0, function(self) {
+  return self.pop();
+});
+
+$.define_method($c.Array, 'push', 1, function(self, obj) {
+  self.push(obj);
+
+  return self;
+});
+$.alias_method($c.Array, '<<', 'push');
+
+$.define_method($c.Array, 'shift', 0, function(self) {
+  return self.shift();
+});
+
+$.define_method($c.Array, 'to_ary', 0, function(self) {
+  return self;
+});
+$.alias_method($c.Array, 'to_a', 'to_ary');
+
+$.define_method($c.Array, 'to_s', 0, function(self) {
+  var desc = [];
+  for(var i = 0; i < self.length; i++) {
+    desc.push(this.funcall(self[i], 'inspect').value);
+  }
+  return this.string_new("[" + desc.join(', ') + "]");
+});
+
+$.define_method($c.Array, 'unshift', 1, function(self, obj) {
+  self.unshift(obj);
+  return self;
+});
+
+$.define_method($c.Array, 'uniq!', 0, function(self) {
+  // TODO
+  return self;
 });
 
 Array.prototype.klass = $c.Array;
