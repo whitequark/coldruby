998ca90cac7d327c1ccff7aa0d0e9022541913cc
Peter Zotov <whitequark@whitequark.org>
2011-04-28 21:10:56 +0400

    Implemented a lot of Fixnum methods.

--- runtime/10_fixnum.js
+++ runtime/10_fixnum.js
@@ -8,6 +8,10 @@ $.define_method($c.Fixnum, 'to_f', 0, function(self) {
   return $.float_new(self);
 });
 
+$.define_method($c.Fixnum, '-@', 1, function(self) {
+  return -self;
+});
+
 $.define_method($c.Fixnum, '+', 1, function(self, other) {
   return self + this.to_int(other);
 });
@@ -34,6 +38,47 @@ $.define_method($c.Fixnum, '%', 1, function(self, other) {
     this.raise(this.e.ZeroDivisionError, "divided by 0");
 
   return self % other;
+});
+$.alias_method($c.Fixnum, 'modulo', '%');
+
+$.define_method($c.Fixnum, '**', 1, function(self, power) {
+  if(typeof power == "number" && power > 0) {
+    return Math.floor(Math.pow(self, power));
+  } else {
+    return this.float_new(Math.pow(self, this.to_float(power).value));
+  }
+});
+
+$.define_method($c.Fixnum, '~', 1, function(self) {
+  return ~self;
+});
+
+$.define_method($c.Fixnum, '&', 1, function(self, other) {
+  return self & this.to_int(other);
+});
+
+$.define_method($c.Fixnum, '|', 1, function(self, other) {
+  return self | this.to_int(other);
+});
+
+$.define_method($c.Fixnum, '^', 1, function(self, other) {
+  return self ^ this.to_int(other);
+});
+
+$.define_method($c.Fixnum, '[]', 1, function(self, bit) {
+  return (self >> this.to_int(bit)) & 1;
+});
+
+$.define_method($c.Fixnum, 'abs', 0, function(self) {
+  if(self < 0)
+    return -self;
+
+  return self;
+});
+$.alias_method($c.Fixnum, 'magnitude', 'abs');
+
+$.define_method($c.Fixnum, 'zero?', 0, function(self) {
+  return (self == 0) ? Qtrue : Qfalse;
 });
 
 $.define_method($c.Fixnum, '<=>', 1, function(self, other) {
