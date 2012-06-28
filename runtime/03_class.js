8e9c2910e624610d11b281f05cffe61c0342b44c
Peter Zotov <whitequark@whitequark.org>
2012-06-28 20:26:18 +0400

    Implement Class#new.

--- runtime/03_class.js
+++ runtime/03_class.js
@@ -32,6 +32,44 @@ $.define_method($c.Class, 'new', -1, function(self, args) {
   return object;
 });
 
+$.define_singleton_method($c.Class, 'new', -1, function(self, args) {
+  var sf = this.context.sf;
+
+  this.check_args(args, 0, 1);
+  var superklass = args[0];
+
+  if(superklass && superklass.type == 'singleton')
+    this.raise(this.e.TypeError, "can't make subclass of singleton class");
+
+  var klass = {
+    klass:             this.internal_constants.Class,
+    superklass:        superklass ? superklass : this.internal_constants.Object,
+    singleton_klass:   null,
+    constants:         {},
+    class_variables:   {},
+    instance_methods:  {},
+    ivs:               {},
+    toString:          function() { return "#<Class: " + $.class2name(this) + ">"; }
+  };
+
+  var cref = [ klass ];
+  for(var i = 0; i < sf.block.stack_frame.cref.length; i++) {
+    cref.push(sf.block.stack_frame.cref[i]);
+  }
+
+  var sf_opts = {
+    self: klass,
+    ddef: klass,
+    cref: cref,
+
+    outer: sf.block.stack_frame
+  };
+
+  this.execute(sf_opts, sf.block, []);
+
+  return klass;
+});
+
 $.define_method($c.Class, 'superclass', 0, function(self) {
   return self.superklass == null ? Qnil : self.superklass;
 });
